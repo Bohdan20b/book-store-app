7dfaa3df730984db5fc8cfbc902b0b096337d4c5
Bohdan Senyk <senyk2001@gmail.com>
2024-01-15 16:39:06 +0200

    minor fixes

--- src/main/java/com/example/bookstore/model/Book.java
+++ src/main/java/com/example/bookstore/model/Book.java
@@ -7,11 +7,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 @Entity
 @Table(name = "books")
 public class Book {
@@ -28,5 +26,4 @@ public class Book {
     private BigDecimal price;
     private String description;
     private String coverImage;
-
 }
