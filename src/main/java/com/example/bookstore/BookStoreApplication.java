5bc8b64ffe9c3de66a779474cf51b4b8332232a4
Bohdan Senyk <senyk2001@gmail.com>
2024-01-15 20:02:48 +0200

    removed commandLineRunner

--- src/main/java/com/example/bookstore/BookStoreApplication.java
+++ src/main/java/com/example/bookstore/BookStoreApplication.java
@@ -1,13 +1,9 @@
 package com.example.bookstore;
 
-import com.example.bookstore.model.Book;
 import com.example.bookstore.service.BookService;
-import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
@@ -17,20 +13,4 @@ public class BookStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            Book book = new Book();
-            book.setAuthor("William Shakespeare");
-            book.setTitle("Macbeth");
-            book.setDescription("description example");
-            book.setPrice(BigDecimal.valueOf(4.69));
-            book.setIsbn("ibsn string example");
-            book.setCoverImage("cover image");
-            bookService.save(book);
-            System.out.println(bookService.findAll());
-        };
-    }
-
 }
