a486c12c81ad74b217c82e573478a3419632014b
Bohdan Senyk <senyk2001@gmail.com>
2024-01-23 14:43:34 +0200

    implemented pagination

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -6,6 +6,7 @@ import com.example.bookstore.dto.CreateBookRequestDto;
 import com.example.bookstore.service.BookService;
 import jakarta.validation.Valid;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,8 +28,8 @@ public class BookController {
     }
 
     @GetMapping
-    public List<BookDto> findAll() {
-        return bookService.findAll();
+    public List<BookDto> findAll(Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
