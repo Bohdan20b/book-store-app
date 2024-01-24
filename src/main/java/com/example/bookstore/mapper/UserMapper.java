abb9040da17fd1d1a64ce892e8497f835931392f
Bohdan Senyk <senyk2001@gmail.com>
2024-01-24 15:23:14 +0200

    implemented UserMapper

---
+++ src/main/java/com/example/bookstore/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.example.bookstore.mapper;
+
+import com.example.bookstore.config.MapperConfig;
+import com.example.bookstore.dto.user.UserDto;
+import com.example.bookstore.dto.user.UserRegistrationRequestDto;
+import com.example.bookstore.model.User;
+import org.mapstruct.Mapper;
+
+@Mapper(config = MapperConfig.class)
+public interface UserMapper {
+    UserDto toDto(User user);
+
+    User toModel(UserRegistrationRequestDto dto);
+}
