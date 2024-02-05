c6c69284a876f510e02fc1ec78b7ceab42fd6776
Bohdan Senyk <senyk2001@gmail.com>
2024-02-05 13:55:44 +0200

    added findAllByCategoryId custom query

--- src/main/java/com/example/bookstore/repository/BookRepository.java
+++ src/main/java/com/example/bookstore/repository/BookRepository.java
@@ -1,8 +1,15 @@
 package com.example.bookstore.repository;
 
 import com.example.bookstore.model.Book;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+    @Query("FROM Book b INNER JOIN FETCH b.categories c WHERE c.id = :category_id")
+    List<Book> findAllByCategoryId(@Param("category_id") Long categoryId);
 }
