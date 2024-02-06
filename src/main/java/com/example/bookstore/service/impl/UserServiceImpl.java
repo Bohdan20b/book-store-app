db80ac002a958183253d66da487d6bc3111fc3c2
Bohdan Senyk <senyk2001@gmail.com>
2024-02-06 16:36:31 +0200

    fixing packages

--- src/main/java/com/example/bookstore/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/bookstore/service/impl/UserServiceImpl.java
@@ -1,4 +1,4 @@
-package com.example.bookstore.service;
+package com.example.bookstore.service.impl;
 
 import com.example.bookstore.dto.user.UserDto;
 import com.example.bookstore.dto.user.UserRegistrationRequestDto;
@@ -6,6 +6,7 @@ import com.example.bookstore.exception.RegistrationException;
 import com.example.bookstore.mapper.UserMapper;
 import com.example.bookstore.model.User;
 import com.example.bookstore.repository.UserRepository;
+import com.example.bookstore.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
