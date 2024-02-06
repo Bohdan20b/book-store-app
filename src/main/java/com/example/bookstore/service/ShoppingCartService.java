012582758c175ab64348638e662e1a2ccac519f5
Bohdan Senyk <senyk2001@gmail.com>
2024-02-06 16:38:38 +0200

    implementing ShoppingCartService

---
+++ src/main/java/com/example/bookstore/service/ShoppingCartService.java
@@ -0,0 +1,14 @@
+package com.example.bookstore.service;
+
+import com.example.bookstore.dto.cartitem.CartItemRequestDto;
+import com.example.bookstore.dto.shoppingcart.ShoppingCartDto;
+
+public interface ShoppingCartService {
+    ShoppingCartDto getShoppingCartDto(String email);
+
+    ShoppingCartDto addItemsToCart(String email, CartItemRequestDto requestDto);
+
+    ShoppingCartDto updateQuantity(Long id, CartItemRequestDto requestDto);
+
+    void deleteCartById(Long id);
+}
