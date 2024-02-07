d48d7ae2a47a33f702d0cd0f49c0de171adae7c6
Bohdan Senyk <senyk2001@gmail.com>
2024-02-07 18:40:34 +0200

    impplemented sorting

--- src/main/java/com/example/bookstore/controller/CategoryController.java
+++ src/main/java/com/example/bookstore/controller/CategoryController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -42,8 +43,8 @@ public class CategoryController {
     @Operation(summary = "Get all categories", description = "Get a list of categories")
     @PreAuthorize("hasAuthority('USER')")
     @GetMapping
-    public List<CategoryDto> getAll() {
-        return categoryService.findAll();
+    public List<CategoryDto> getAll(Pageable pageable) {
+        return categoryService.findAll(pageable);
     }
 
     @Operation(summary = "Get a category by id", description = "Get a specific category by id")
@@ -72,8 +73,9 @@ public class CategoryController {
     @Operation(summary = "Get books by category id", description = "Get books by category id")
     @PreAuthorize("hasAuthority('USER')")
     @GetMapping("/{id}/books")
-    public List<BookDtoWithoutCategoryIds> getBooksByCategoryId(@PathVariable Long id) {
-        return bookService.findAllByCategoryId(id)
+    public List<BookDtoWithoutCategoryIds> getBooksByCategoryId(@PathVariable Long id,
+            Pageable pageable) {
+        return bookService.findAllByCategoryId(id, pageable)
                 .stream()
                 .map(bookMapper::toDtoWithoutCategoryIds)
                 .toList();
