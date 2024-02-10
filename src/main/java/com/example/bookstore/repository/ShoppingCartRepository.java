20989df2927e18427ccc12bfee70b71c12deece7
Bohdan Senyk <senyk2001@gmail.com>
2024-02-10 19:31:09 +0200

    fixing checkstyle

--- src/main/java/com/example/bookstore/repository/ShoppingCartRepository.java
+++ src/main/java/com/example/bookstore/repository/ShoppingCartRepository.java
@@ -1,13 +1,13 @@
 package com.example.bookstore.repository;
 
 import com.example.bookstore.model.ShoppingCart;
-import com.example.bookstore.model.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
-    @Query("SELECT DISTINCT sc FROM ShoppingCart sc LEFT JOIN FETCH sc.cartItems ci WHERE sc.user.email = :email")
+    @Query("SELECT DISTINCT sc FROM ShoppingCart sc "
+            + "LEFT JOIN FETCH sc.cartItems ci WHERE sc.user.email = :email")
     Optional<ShoppingCart> findByUserWithItems(@Param("email") String email);
 }
